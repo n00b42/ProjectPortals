8d63b3599337444fe6863785cebbea9ab51f67f7
TrenTech <TrenTech@gmail.com>
2016-01-04 02:03:39 -0500

    Set remaining on home remove

--- src/com/gmail/trentech/pjp/commands/home/CMDRemove.java
+++ src/com/gmail/trentech/pjp/commands/home/CMDRemove.java
@@ -37,6 +37,7 @@ public class CMDRemove implements CommandExecutor {
 			return CommandResult.empty();
 		}
 		
+		config.getNode("Remaining").setValue(config.getNode("Remaining").getInt() + 1);
 		config.getNode("Homes").removeChild(homeName);
 
 		configManager.save();
