757c8926d9f0c08583302320ba66ba032b55a0f8
TrenTech@gmail.com <TrenTech@gmail.com>
2016-03-09 23:04:28 -0500

    Fix gradle, update @Plugin to generate mcmod.info

--- src/com/gmail/trentech/pjp/utils/Resource.java
+++ src/com/gmail/trentech/pjp/utils/Resource.java
@@ -3,7 +3,10 @@ package com.gmail.trentech.pjp.utils;
 public class Resource {
 
 	public final static String NAME = "Project Portals";
-	public final static String VERSION = "0.10.42";
-	public final static String ID = "pjp";
+	public final static String VERSION = "0.10.43";
+	public final static String ID = "com.gmail.trentech.pjp";
+	public final static String AUTHOR = "TrenTech";
+	public final static String URL = "http://github.com/TrenTech/ProjectPortals";
+	public final static String DESCRIPTION = "One stop shop for all things portals";
 
 }
