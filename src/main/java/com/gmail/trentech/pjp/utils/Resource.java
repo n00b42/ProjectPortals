0fe1905d09210c37f3fa2dc7f9edae4d4b85aa24
TrenTech@gmail.com <TrenTech@gmail.com>
2016-06-10 10:33:59 -0400

    SpongeGradle

--- src/main/java/com/gmail/trentech/pjp/utils/Resource.java
+++ src/main/java/com/gmail/trentech/pjp/utils/Resource.java
@@ -3,10 +3,9 @@ package com.gmail.trentech.pjp.utils;
 public class Resource {
 
 	public final static String NAME = "Project Portals";
-	public final static String VERSION = "0.11.1";
+	public final static String VERSION = "@project.version@";
 	public final static String ID = "com.gmail.trentech.pjp";
 	public final static String AUTHOR = "TrenTech";
 	public final static String URL = "http://github.com/TrenTech/ProjectPortals";
-	public final static String DESCRIPTION = "One stop shop for all things portals";
 
 }
