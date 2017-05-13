cbeec59b042cb0f13fd7941b0a5283406d8f34b4
trentech <trentech@gmail.com>
2017-05-13 07:57:02 -0400

    Update CMDPjp.java

--- src/main/java/com/gmail/trentech/pjp/commands/CMDPjp.java
+++ src/main/java/com/gmail/trentech/pjp/commands/CMDPjp.java
@@ -21,42 +21,38 @@ public class CMDPjp implements CommandExecutor {
 	@Override
 	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
 		ConfigurationNode node = ConfigManager.get(Main.getPlugin()).getConfig().getNode("settings", "modules");
-		
-		if (Sponge.getPluginManager().isLoaded("helpme")) {
-			List<Help> commands = new ArrayList<>();
-			
-			if (node.getNode("portals").getBoolean()) {
-				commands.add(Help.get("portal").get());
-			}
-			if (node.getNode("plates").getBoolean()) {
-				commands.add(Help.get("plate").get());
-			}
-			if (node.getNode("levers").getBoolean()) {
-				commands.add(Help.get("lever").get());
-			}
-			if (node.getNode("signs").getBoolean()) {
-				commands.add(Help.get("sign").get());
-			}
-			if (node.getNode("doors").getBoolean()) {
-				commands.add(Help.get("door").get());
-			}
-			if (node.getNode("buttons").getBoolean()) {
-				commands.add(Help.get("button").get());
-			}
-			if (node.getNode("homes").getBoolean()) {
-				commands.add(Help.get("home").get());
-			}
-			if (node.getNode("warps").getBoolean()) {
-				commands.add(Help.get("warp").get());
-			}
-			if (node.getNode("back").getBoolean()) {
-				commands.add(Help.get("back").get());
-			}
 
-			Help.executeList(src, commands);
-			
-			return CommandResult.success();
+		List<Help> commands = new ArrayList<>();
+
+		if (node.getNode("portals").getBoolean()) {
+			commands.add(Help.get("portal").get());
 		}
+		if (node.getNode("plates").getBoolean()) {
+			commands.add(Help.get("plate").get());
+		}
+		if (node.getNode("levers").getBoolean()) {
+			commands.add(Help.get("lever").get());
+		}
+		if (node.getNode("signs").getBoolean()) {
+			commands.add(Help.get("sign").get());
+		}
+		if (node.getNode("doors").getBoolean()) {
+			commands.add(Help.get("door").get());
+		}
+		if (node.getNode("buttons").getBoolean()) {
+			commands.add(Help.get("button").get());
+		}
+		if (node.getNode("homes").getBoolean()) {
+			commands.add(Help.get("home").get());
+		}
+		if (node.getNode("warps").getBoolean()) {
+			commands.add(Help.get("warp").get());
+		}
+		if (node.getNode("back").getBoolean()) {
+			commands.add(Help.get("back").get());
+		}
+
+		Help.executeList(src, commands);
 
 		return CommandResult.success();
 	}
